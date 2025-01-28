e2d259a75cf0457c1527b64b33e81f7fa62c23d5
Rojina Bhandari <rbhandari17@myseneca.ca>
2025-01-28 17:40:59 -0500

    Added lab2d

---
+++ lab2d.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import sys
+
+# Check for the correct number of arguments
+if len(sys.argv) != 3:
+    print(f"Usage: {sys.argv[0]} name age")
+    sys.exit()
+
+# Assign the arguments to name and age
+name = sys.argv[1]
+age = sys.argv[2]
+
+# Print the message
+print(f"Hi {name}, you are {age} years old.")
+
