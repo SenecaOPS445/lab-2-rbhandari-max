4aae374a90322b3e672ab75e062273a31b2ca5fa
Rojina Bhandari <rbhandari17@myseneca.ca>
2025-01-28 17:41:17 -0500

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Author: Your Full Name
+# Author ID: Your Seneca ID
+# Date Created: 2025/01/27
+
+timer = 10
+while timer > 0:
+    print(timer)
+    timer -= 1  # Same as: timer = timer - 1
+print("blast off!")
